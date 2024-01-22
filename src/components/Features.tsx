e539eeba11f604429ff0e3eb7db508739fa45f05
James Harrison <somtochukwujames2000@gmail.com>
2024-01-22 13:53:57 +0100

    chores: added features

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,7 +1,46 @@
 "use client";
+import React, { useState, useRef } from "react";
+import styles from "./styles/features.module.scss";
 
 const Features = () => {
-  return <div>Features</div>;
+  return (
+    <div className={styles.features_container}>
+      <div className={styles.features_header}>
+        <h3>Make payments easily without Hassel</h3>
+      </div>
+      <div className={styles.features_cards}>
+        <div className={styles.features_card}>
+          <img src="./stars.svg" className={styles.card_starone} />
+          <div className={styles.card_texts}>
+            <h3>Payment of Solar-Power with Fiat Currency</h3>
+            <p>
+              Empower your sustainable journey with our cutting-edge feature
+              that allows users to seamlessly renew their solar-power utility using
+              traditional Fiat currency. By integrating these payment options,
+              we provide flexibility and accessibility to a broader spectrum of
+              users, ensuring a more inclusive and sustainable future.
+            </p>
+          </div>
+          <img src="./nairacoins.svg" className={styles.card_coins} />
+        </div>
+        <div className={styles.features_card}>
+          <img src="./stars.svg" className={styles.card_startwo} />
+          <div className={styles.card_texts}>
+            <h3>Payment of Solar-Power with Crypto Currency</h3>
+            <p>
+              Empower your sustainable journey with our cutting-edge feature
+              that allows users to seamlessly renew their solar-power utility
+              using revolutionary world of Crypto Currency. By integrating these
+              payment options, we provide flexibility and accessibility to a
+              broader spectrum of users, ensuring a more inclusive and
+              sustainable future.
+            </p>
+          </div>
+          <img src="./dollarcoins.svg" className={styles.card_coins} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Features;
