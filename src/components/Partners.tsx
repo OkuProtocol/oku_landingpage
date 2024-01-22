32a58f4eb713891f239e26b20e5c2ecc94c25bbe
James Harrison <somtochukwujames2000@gmail.com>
2024-01-22 04:35:24 +0100

    chores: added partners

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -1,7 +1,16 @@
 "use client";
-
+import styles from "./styles/partners.module.scss"
 const Partners = () => {
-  return <div>Partners</div>;
+  return (
+    <div className={styles.partners}>
+      <h3>Partners</h3>
+
+      <div className={styles.partners_image}>
+        <img src="./meter.svg" />
+        <img src="./iotex.svg" />
+      </div>
+    </div>
+  );
 };
 
 export default Partners;
